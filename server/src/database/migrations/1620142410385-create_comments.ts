7f07c6fe7979cd5d3f14e6ae0f728b9cb785c435
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-04 11:56:22 -0400

    Create comments migration

---
+++ server/src/database/migrations/1620142410385-create_comments.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createComments1620142410385 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'comments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true
+          },
+          {
+            name: 'text',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('comments')
+  }
+}
