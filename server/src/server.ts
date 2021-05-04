4a40f254cc1d5f0f5334fbabfdf349982273eda4
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-04 18:09:38 -0400

    added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import './database'
 import { routes } from './routes'
@@ -6,6 +7,7 @@ import { routes } from './routes'
 import './containers'
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 
